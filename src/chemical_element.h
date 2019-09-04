f9432f4f3f65a0bdca29d424e0bb48dc749b6125
Alexander Elfimow <my.free.mail@gmx.de>
2019-09-04 20:57:56 +0200

    Added header file

---
+++ src/chemical_element.h
@@ -0,0 +1,32 @@
+#ifdef CHEMICAL_ELEMENT_H
+#error Already included
+#else
+#define CHEMICAL_ELEMENT_H
+
+class chemical_element
+{
+    public:
+        using atomic_number_t = size_t;
+        using symbol_t = std::string;
+        using atomic_mass_t = double;
+        using state_t = std::string;
+        using stability_t = std::string;
+        using name_t = std::string;
+        using group_t = std::string;
+        using electron_config_t = std::string;
+
+    public:
+        chemical_element() { }
+        virtual ~chemical_element() { }
+
+        virtual atomic_number_t atomic_number() const = 0;
+        virtual symbol_t symbol() const = 0;
+        virtual name_t name() const = 0;
+        virtual atomic_mass_t atomic_mass() const = 0;
+        virtual state_t state() const = 0;
+        virtual stability_t stability() const = 0;
+        virtual group_t group() const = 0;
+        virtual electron_config_t electron_config() const = 0;
+};
+
+#endif
