c929c3f352752b3a1f49deb1de4409bfc807784e
Alexander Elfimow <my.free.mail@gmx.de>
2019-09-10 21:38:56 +0200

    Added override

--- src/hydrogen.h
+++ src/hydrogen.h
@@ -10,14 +10,14 @@ class hydrogen: public chemical_element
         hydrogen() { }
         ~hydrogen() { }
 
-        atomic_number_t atomic_number() const;
-        symbol_t symbol() const;
-        name_t name() const;
-        atomic_mass_t atomic_weight() const;
-        state_t state() const;
-        stability_t stability() const;
-        group_t group() const;
-        electron_config_t electron_config() const;
+        atomic_number_t atomic_number() const override;
+        symbol_t symbol() const override;
+        name_t name() const override;
+        atomic_mass_t atomic_weight() const override;
+        state_t state() const override;
+        stability_t stability() const override;
+        group_t group() const override;
+        electron_config_t electron_config() const override;
 };
 
 #endif
