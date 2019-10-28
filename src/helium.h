653321fa80ef5ba02f58383340934eae5f18891f
Alexander Elfimow <my.free.mail@gmx.de>
2019-10-28 18:53:22 +0100

    Removed further cpp file

--- src/helium.h
+++ src/helium.h
@@ -10,14 +10,45 @@ class helium: public chemical_element
         helium() { }
         ~helium() { }
 
-        atomic_number_t atomic_number() const override;
-        symbol_t symbol() const override;
-        name_t name() const override;
-        atomic_mass_t atomic_weight() const override;
-        state_t state() const override;
-        stability_t stability() const override;
-        group_t group() const override;
-        electron_config_t electron_config() const override;
+        atomic_number_t atomic_number() const override
+        {
+            return 2;
+        }
+
+        symbol_t symbol() const override
+        {
+            return "He";
+        }
+
+        name_t name() const override
+        {
+            return "helium";
+        }
+
+        atomic_mass_t atomic_weight() const override
+        {
+            return 4.002602;
+        }
+
+        state_t state() const override
+        {
+            return "gas";
+        }
+
+        stability_t stability() const override
+        {
+            return "stable";
+        }
+
+        group_t group() const override
+        {
+            return "18: noble gases";
+        }
+
+        electron_config_t electron_config() const override
+        {
+            return "1s^2";
+        }
 };
 
 #endif
