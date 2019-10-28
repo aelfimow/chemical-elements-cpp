d9c01c1e5f25b0f5f2d05b93004f8bd072c62e59
Alexander Elfimow <my.free.mail@gmx.de>
2019-10-28 18:51:58 +0100

    Removed cpp file

--- src/hydrogen.h
+++ src/hydrogen.h
@@ -10,14 +10,45 @@ class hydrogen: public chemical_element
         hydrogen() { }
         ~hydrogen() { }
 
-        atomic_number_t atomic_number() const override;
-        symbol_t symbol() const override;
-        name_t name() const override;
-        atomic_mass_t atomic_weight() const override;
-        state_t state() const override;
-        stability_t stability() const override;
-        group_t group() const override;
-        electron_config_t electron_config() const override;
+        atomic_number_t atomic_number() const override
+        {
+            return 1;
+        }
+
+        symbol_t symbol() const override
+        {
+            return "H";
+        }
+
+        name_t name() const override
+        {
+            return "hydrogen";
+        }
+
+        atomic_mass_t atomic_weight() const override
+        {
+            return 1.008;
+        }
+
+        state_t state() const override
+        {
+            return "gas";
+        }
+
+        stability_t stability() const override
+        {
+            return "stable";
+        }
+
+        group_t group() const override
+        {
+            return "1: H and alkali metals";
+        }
+
+        electron_config_t electron_config() const override
+        {
+            return "1s^1";
+        }
 };
 
 #endif
